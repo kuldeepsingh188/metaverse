94a78b1e1c6382f6b1daabc42933a95c6c66199b
kuldeepsingh18 <you@example.com>
2023-11-27 14:27:23 +0530

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var backToTopButton = document.getElementById("back-to-top");
+
+    // Show/hide the button based on the scroll position
+    window.onscroll = function () {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    };
+
+    // Scroll to the top when the button is clicked
+    backToTopButton.addEventListener("click", function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
+});
+
+setTimeout(function () {
+        document.querySelector('.preloader').style.display = 'none';
+            // Add your code to display the actual content here
+}, 9000); // Adjust the timeout duration as needed
+
+setTimeout(() => {
+    document.body.classList.remove("overflow-hidden");
+    document.getElementById("pre-loader").style.display = "none";
+}, 2500);
+// JavaScript to handle the responsive navigation menu
+function show() {
+    document.getElementById("navMenu").classList.add("!start-0");
+    document.body.classList.add("overflow-hidden");
+}
